292e94920226e661071285268016230070d38bca
RichardZhiLi <948945081@qq.com>
2022-04-29 22:01:12 +0100

    Implemented static index page.
    1.Implemented the navigation between login page and index page by importing react-router.
    
    2.Implemented the static index page.
    
    3.Deleted some useless static files in the frontend directory.

--- frontend/src/Login/Login.js
+++ frontend/src/Login/Login.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react'
-import {Input,Button} from 'antd'
+import {Input,Button, Modal} from 'antd'
+import {UserOutlined} from '@ant-design/icons'
+import { useNavigate } from 'react-router-dom'
 import './Login.css'
+
 class Login extends Component {
-    inputUniNum = e=>{
+    handleInput = e=>{
+        //handle the input event
         let input = e.target.value;
         if(input.length >= 8 ) input = input.substr(0,8)
         this.setState({inputValue:input})
     }
+    handleSubmit = e=>{
+        //handle the submission event, validate the input from user
+        const input = this.state.inputValue;
+        if(input.length !== 8) {
+            return Modal.error({
+                content:<div><p>Please input a 8 digit number</p></div>
+            })
+        }
+        const {navigate} = this
+        navigate('/index',{replace:true})
+    }
     constructor(props){
         super(props)
+        //bind the navigation method to the global instance 
+        this.navigate = props.navigate
         this.state = {
             inputValue:''
         }
@@ -23,13 +40,30 @@ class Login extends Component {
         value={this.state.inputValue}
         className='loginInput'
         size='large'
-        onInput={this.inputUniNum}
+        onInput={this.handleInput}
+        prefix={<UserOutlined></UserOutlined>}
         >
         </Input>
-        <Button shape="round" size='large' block>Submit</Button>
+        <Button 
+        type ='primary' 
+        shape="round" 
+        size='large' 
+        block 
+        ghost
+        onClick={this.handleSubmit}
+        >
+        Submit
+        </Button>
             </div>
         </div>
         return layout
     }
 }
-export default Login
+export default ()=>{
+    /**
+     * In react-router, useNavigate is a hook funciton, 
+     *      if we wanna use it, then we need a wrapper function outside of the class
+     * */
+    const navigation = useNavigate();
+    return (<Login navigate={navigation}></Login>); 
+}
